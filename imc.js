61aff21ac84b19b3e0b0a36ed72493a9b85e88fd
Lucas Alonso <111199312+Lucas-Alonso11@users.noreply.github.com>
2022-11-27 16:26:32 -0300

    criando o imc.js

---
+++ imc.js
@@ -0,0 +1,5 @@
+const nome = document.addEventListener('nome');
+const altura = document.addEventListener('altura');
+const peso = document.addEventListener('peso');
+const calcular = document.addEventListener('calcular');
+const resultado = document.addEventListener('resultado');
