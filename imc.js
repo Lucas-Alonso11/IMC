771d2dd0eb088c01e4a0e41623816ee4006fab03
Lucas Alonso <111199312+Lucas-Alonso11@users.noreply.github.com>
2022-12-09 17:55:49 -0300

    update main.js

--- imc.js
+++ imc.js
@@ -1,4 +1,27 @@
-    const nome = document.getElementById('nome').value;
-    const altura = document.getElementById('altura').value;
-    const peso = document.getElementById('peso').value;
-    const resultado = document.getElementById('resultado');
+const resultado = document.querySelector('#resultado')
+const altura = document.querySelector('#altura')
+const peso = document.querySelector('#peso')
+
+const calcIMC = () => {
+  if (peso.value / (altura.value * altura.value)).toFixed(2)
+  let classification = ''
+
+  if (imc < 18.5) {
+    classification = 'Abaixo do Peso'
+  } else if (imc < 25) {
+    classification = 'Peso Normal'
+  } else if (imc < 30) {
+    classification = 'Acima do Peso'
+  } else if (icm < 35) {
+    classification = 'Obesidade Grau I'
+  } else if (imc < 41) {
+    classification = 'Obesidade Grau II'
+  } else {
+    classification = 'Obesidade Grau III'
+  }
+
+  resultado.innerHTML = 'IMC: ${imc} (${classification})'
+    else {
+      resultado.innerHTML = 'Preencha os camps'
+    }
+}
