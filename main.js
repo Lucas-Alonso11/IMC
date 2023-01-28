657ff839e381c8f2b53ec88c8f99b7f9ecdf7aef
Lucas Alonso <111199312+Lucas-Alonso11@users.noreply.github.com>
2023-01-28 10:57:05 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,26 @@
+function calcular () {
+    let altura = document.getElementById('txta')
+    let peso = document.getElementById('txtp')
+    let res = document.querySelector('div#res')
+    
+
+    if (altura.value.length == 0 || peso.value.length == 0) {
+        window.alert('Digite um numero')
+        res.innerHTML = 'Impossivel contar'
+    } else {
+        let a = Number(altura.value)
+        let p = Number(peso.value)
+        let imc = p / (a*a)
+        imc = imc.toFixed(2)
+        res.innerHTML = `O Seu IMC é: ${imc}</br>`
+        
+        if (imc <= 18.5) {
+            res.innerHTML += 'Baixo do Peso'
+        }else if (imc > 18.5 && imc <= 24.99) {
+            res.innerHTML += 'Normal'
+        } else if ( imc >= 25 && imc <= 29.99) {
+            res.innerHTML += 'Sobrepeso'
+        }
+    } 
+        
+}
